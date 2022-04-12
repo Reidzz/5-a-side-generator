c437b3b92a3216b75639d22c271ac9c62bab5680
Reidzz <danielreid2007@hotmail.co.uk>
2022-04-12 22:27:40 +0100

    Used the .push() method to insert values into the array. Changed the forEach loop to the for loop to allow index manipulation of the array

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 const teamList = []
-/*let i = 0*/
-/*const totalTeams = teamList.length*/ // Why does this variable not work unless it is called in the function?
-
 const submitBtn = document.querySelector("#submit")
+const formInput = document.querySelector("#team")
 const form = document.querySelector(".team-form")
-const messageContainer = document.querySelector(".team-list")
+const teamContainerLeft = document.querySelector(".left")
+const teamContainerRight = document.querySelector(".right")
 
 submitBtn.addEventListener("click", addTeam)
 
 function addTeam() {
+    if (formInput.value === "") {
+        /*formMessage.innerHTML = warning
+        setTimeout(clearMessage, 3000)*/
+        return false
+    }
     userInput = document.getElementById("team").value
     totalTeams = teamList.push(userInput) // .push() method can return the array length
     /*form.reset()*/
-    /*teamList[i] = document.getElementById("team").value
-    i++
-    form.reset()*/
-    /*const totalTeams = teamList.length*/
     if (totalTeams >= 10) {
         submitBtn.removeEventListener("click", addTeam)
         displayTeams()
     }
     console.log(userInput)
-    console.log(teamList)
+    /*console.log(teamList)*/
     /*console.log(totalTeams)*/
 }
 
 function displayTeams() {
-    teamList.forEach((item) => {
+    for (i = 0; i < teamList.length; i++) {
         let li = document.createElement("li")
-        li.innerText = item
-        messageContainer.appendChild(li)
-    })
+        li.innerText = `${teamList[i]}versus`
+        teamContainerLeft.appendChild(li)
+        if (i === 4) {
+            for (i = 5; i < teamList.length; i++) {
+                let li = document.createElement("li")
+                li.innerText = `${teamList[i]}`
+                teamContainerRight.appendChild(li)
+            }
+            break
+        }
+        console.log(teamList)
+    }
 }
 
 // When the list reaches 5(4) we want the the remaining to be split into a column beside it
