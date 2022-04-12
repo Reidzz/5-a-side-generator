eea48cbde38df501d9a8fd6d5a9efdcbd99b1156
Reidzz <danielreid2007@hotmail.co.uk>
2022-04-12 22:06:01 +0100

    Revert "Added in team columns"
    This reverts commit acdad494962d8650947685ee6330029e2e29dd95.

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
 const teamList = []
+/*let i = 0*/
+/*const totalTeams = teamList.length*/ // Why does this variable not work unless it is called in the function?
+
 const submitBtn = document.querySelector("#submit")
-const formInput = document.querySelector("#team")
 const form = document.querySelector(".team-form")
-const teamContainerLeft = document.querySelector(".left")
-const teamContainerRight = document.querySelector(".right")
+const messageContainer = document.querySelector(".team-list")
 
 submitBtn.addEventListener("click", addTeam)
 
 function addTeam() {
-    if (formInput.value === "") {
-        /*formMessage.innerHTML = warning
-        setTimeout(clearMessage, 3000)*/
-        return false
-    }
     userInput = document.getElementById("team").value
     totalTeams = teamList.push(userInput) // .push() method can return the array length
     /*form.reset()*/
+    /*teamList[i] = document.getElementById("team").value
+    i++
+    form.reset()*/
+    /*const totalTeams = teamList.length*/
     if (totalTeams >= 10) {
         submitBtn.removeEventListener("click", addTeam)
         displayTeams()
     }
     console.log(userInput)
-    /*console.log(teamList)*/
+    console.log(teamList)
     /*console.log(totalTeams)*/
 }
 
 function displayTeams() {
-    for (i = 0; i < teamList.length; i++) {
+    teamList.forEach((item) => {
         let li = document.createElement("li")
-        li.innerText = `${teamList[i]}versus`
-        teamContainerLeft.appendChild(li)
-        if (i === 4) {
-            for (i = 5; i < teamList.length; i++) {
-                let li = document.createElement("li")
-                li.innerText = `${teamList[i]}`
-                teamContainerRight.appendChild(li)
-            }
-            break
-        }
-        console.log(teamList)
-    }
+        li.innerText = item
+        messageContainer.appendChild(li)
+    })
 }
 
 // When the list reaches 5(4) we want the the remaining to be split into a column beside it
